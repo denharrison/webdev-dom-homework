f38f76134f7e4ea076f6d3cdabbec3c838bf1b77
denharrison <denisharrison033@gmail.com>
2025-03-14 16:11:35 +0300

    Add red

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import { renderComments } from './modules/renderFunctions.js'
 import { updateComments } from './modules/massifs.js'
 import { getComments } from './modules/api.js'
+
+// Проверяем, был ли уже выполнен первый запуск
+if (!localStorage.getItem('isInitialLoad')) {
+    document.getElementById('loading-message').style.display = 'block'
+    localStorage.setItem('isInitialLoad', 'true')
+}
+
 // Первоначальный рендер комментариев
 getComments().then((result) => {
     updateComments(result)
     renderComments()
+    document.getElementById('loading-message').style.display = 'none'
 })
